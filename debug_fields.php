191363610e34b660355f30a659fe50455cc88e1a
chrisdownham <chris@r1se.co.uk>
2025-06-25 19:44:34 +0100

    Fix debug_fields.php: remove stray backslashes

--- debug_fields.php
+++ debug_fields.php
@@ -8,23 +8,23 @@ use WellnessLiving\Wl\Lead\LeadModel;
 use WellnessLiving\WlRegionSid;
 
 // 1. Authenticate
-\$cfg     = ExampleConfig::create(WlRegionSid::US_EAST_1);
-\$notepad = new NotepadModel(\$cfg);
-\$notepad->get();
+$cfg     = ExampleConfig::create(WlRegionSid::US_EAST_1);
+$notepad = new NotepadModel($cfg);
+$notepad->get();
 
-\$enter   = new EnterModel(\$cfg);
-\$enter->cookieSet(\$notepad->cookieGet());
-\$enter->s_login    = \$_ENV['WL_LOGIN'];
-\$enter->s_notepad  = \$notepad->s_notepad;
-\$enter->s_password = \$notepad->hash(\$_ENV['WL_PASSWORD']);
-\$enter->post();
+$enter   = new EnterModel($cfg);
+$enter->cookieSet($notepad->cookieGet());
+$enter->s_login    = $_ENV['WL_LOGIN'];
+$enter->s_notepad  = $notepad->s_notepad;
+$enter->s_password = $notepad->hash($_ENV['WL_PASSWORD']);
+$enter->post();
 
 // 2. Fetch the list of “new-client” fields
-\$lead = new LeadModel(\$cfg);
-\$lead->cookieSet(\$notepad->cookieGet());
-\$lead->k_business = \$_ENV['WL_BUSINESS_ID'];
-\$lead->get();
+$lead = new LeadModel($cfg);
+$lead->cookieSet($notepad->cookieGet());
+$lead->k_business = $_ENV['WL_BUSINESS_ID'];
+$lead->get();
 
-// 3. Dump it as JSON
+// 3. Dump it as pure JSON
 header('Content-Type: application/json', true, 200);
-echo json_encode(\$lead->a_field_list);
+echo json_encode($lead->a_field_list);
