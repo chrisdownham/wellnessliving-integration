2534e37480b254cc5ecc0354ba7bf8590f6d05b8
chrisdownham <chris@r1se.co.uk>
2025-06-25 20:25:38 +0100

    Fix debug_profile_fields: output pure JSON for profile/edit fields

--- debug_profile_fields.php
+++ debug_profile_fields.php
@@ -8,23 +8,23 @@ use WellnessLiving\Wl\Profile\Edit\EditModel;
 use WellnessLiving\WlRegionSid;
 
 // Authenticate
-\$config  = ExampleConfig::create(WlRegionSid::US_EAST_1);
-\$notepad = new NotepadModel(\$config);
-\$notepad->get();
+$config  = ExampleConfig::create(WlRegionSid::US_EAST_1);
+$notepad = new NotepadModel($config);
+$notepad->get();
 
-\$enter   = new EnterModel(\$config);
-\$enter->cookieSet(\$notepad->cookieGet());
-\$enter->s_login    = \$_ENV['WL_LOGIN'];
-\$enter->s_notepad  = \$notepad->s_notepad;
-\$enter->s_password = \$notepad->hash(\$_ENV['WL_PASSWORD']);
-\$enter->post();
+$enter   = new EnterModel($config);
+$enter->cookieSet($notepad->cookieGet());
+$enter->s_login    = $_ENV['WL_LOGIN'];
+$enter->s_notepad  = $notepad->s_notepad;
+$enter->s_password = $notepad->hash($_ENV['WL_PASSWORD']);
+$enter->post();
 
-// Fetch the profile “create client” fields
-\$edit = new EditModel(\$config);
-\$edit->cookieSet(\$notepad->cookieGet());
-\$edit->k_business = \$_ENV['WL_BUSINESS_ID'];
-\$edit->get();
+// Fetch the “create-client” profile fields
+$edit = new EditModel($config);
+$edit->cookieSet($notepad->cookieGet());
+$edit->k_business = $_ENV['WL_BUSINESS_ID'];
+$edit->get();
 
-// Output pure JSON
+// Dump them as pure JSON
 header('Content-Type: application/json', true, 200);
-echo json_encode(\$edit->a_field_list);
+echo json_encode($edit->a_field_list);
