9b4ddea9d11113b15f44da00e7ae58c7b4ad543f
chrisdownham <chris@r1se.co.uk>
2025-06-25 20:08:49 +0100

    Fix debug_fields: output clean JSON of all required fields

--- debug_fields.php
+++ debug_fields.php
@@ -8,23 +8,23 @@ use WellnessLiving\Wl\Lead\LeadModel;
 use WellnessLiving\WlRegionSid;
 
 // 1. Authenticate
-$cfg     = ExampleConfig::create(WlRegionSid::US_EAST_1);
-$notepad = new NotepadModel($cfg);
+$config  = ExampleConfig::create(WlRegionSid::US_EAST_1);
+$notepad = new NotepadModel($config);
 $notepad->get();
 
-$enter   = new EnterModel($cfg);
+$enter = new EnterModel($config);
 $enter->cookieSet($notepad->cookieGet());
 $enter->s_login    = $_ENV['WL_LOGIN'];
 $enter->s_notepad  = $notepad->s_notepad;
 $enter->s_password = $notepad->hash($_ENV['WL_PASSWORD']);
 $enter->post();
 
-// 2. Fetch the list of “new-client” fields
-$lead = new LeadModel($cfg);
+// 2. Fetch the “new client” fields
+$lead = new LeadModel($config);
 $lead->cookieSet($notepad->cookieGet());
 $lead->k_business = $_ENV['WL_BUSINESS_ID'];
 $lead->get();
 
-// 3. Dump it as pure JSON
+// 3. Output as pure JSON
 header('Content-Type: application/json', true, 200);
 echo json_encode($lead->a_field_list);
